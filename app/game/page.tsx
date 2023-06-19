ae0f21d5d961d206110eb667e5bcf4baceaf0ac1
Jon D <jon@neverdesign.net>
2023-06-19 08:13:36 -0600

    Started implementing game and tile

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -6,16 +6,31 @@ import React from "react"
 
 // Data
 import GameData from "../../data/data.js"
+import Tile from "@/components/tiles/Tile.js";
 
 export default class Game extends React.Component {
+    boardSize: number = 500;
+    gridSize: number = this.boardSize/8;
+    tiles: Tile[] = [];
+
     constructor (props:any) {
         console.log('Game: Constructor');
         super(props);
+
+        this.state = {
+            tileData: GameData.tiles
+        }
     }
+
+    generateTiles = () => {
+        console.log('Game: Constructor');
+        let tiles = [];
+        
+    };
     
     render () {
-        return `
+        return (
             <p>Hello</p>
-        `
+        )
     }
 }
--- components/tiles/Tile.ts
+++ components/tiles/Tile.ts
@@ -1,8 +1,15 @@
-export class Tile {
+// Client Side Renderer
+'use client'
+
+import { TileData } from "@/data/data";
+// Imports
+import React from "react"
+
+export default class Tile extends React.Component {
     isDark: boolean = false;
     id: number;
     col: string;
-    row: number;
+    row: string;
     state: {
         active: boolean,
         occupied: boolean,
@@ -10,7 +17,9 @@ export class Tile {
         isInteractive: boolean
     }
 
-    constructor( data:any ){
+    constructor( props:any, data:TileData ){
+        super(props);
+
         // console.log('Tile: constructor:', data);
         this.id = data.id;
         this.col = data.col;
@@ -23,4 +32,33 @@ export class Tile {
         }
         this.isDark = data.isDark;
     }
+
+    /**
+	 * @function Render
+	 * @purpose render the component
+	 */
+	render() {
+		const classColour = this.isDark ? 'dark ' : 'light ';
+		const classActive = this.state.active ? 'active ' : '';
+		const hoverClass = '';
+
+		const size = this.props.size;
+		const style = {
+			tileSize: {
+				width: size,
+				height: size
+			}
+		};
+
+		const { label, key, col, row, children, handleclick, ...rest } = this.props;
+
+		return (
+			<div id={label} key={key} row={row} col={col} className={'tile ' + classColour + classActive} style={style.tileSize} onClick={this.onClick} {...rest}>
+				<div className="inner">
+					<span>{label}</span>
+					{children}
+				</div>
+			</div>
+		);
+	}
 }
