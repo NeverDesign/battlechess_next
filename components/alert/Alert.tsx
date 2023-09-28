2d739d0149f48f806af6b8e5904539d722a82252
Jon D <jon@neverdesign.net>
2023-09-28 03:40:32 +0000

    Alert component implementation pt. 1

--- components/alert/Alert.tsx
+++ components/alert/Alert.tsx
@@ -3,15 +3,33 @@
 
 // Imports
 import styles from './style.module.scss';
-import React from "react"
+import React, { Component, createRef } from "react"
 
 // Types
-type alertProps =  { 
+type alertProps =  {
+    label: string,
+    key: string,
     status: string, 
     type: string, 
     message: string, 
 };
 
-export default class Alert extends React.Component<alertProps> {
+export default class Alert extends Component<alertProps> {
+    ref = createRef();
 
+    constructor ( props: alertProps ){
+        super(props);
+    }
+
+    render(): React.ReactNode {
+        const { label, key, status, type, message, ...rest } = this.props;
+
+		return (
+			<div id={label} key={key} className={'alert state-' + status + ' type-' + type} {...rest} >
+				<div className="inner">
+					<span>{ message }</span>
+				</div>
+			</div>
+		);
+    }
 }
